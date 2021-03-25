dcb7f1c04bf13efac4e7121b3cd85d1795cc9cbd
Kruthika K <kruthikakalmali@gmail.com>
2021-03-25 21:24:01 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+# EDIT
+USERNAME = ''
+PASSWORD = ''
+COURSES = [
+    'piano-lessons-teach-yourself-to-play'
+]
+
+COLLECTIONS = [
+
+]
+
+# EDIT IF YOU NEED TO
+BASE_DOWNLOAD_PATH = os.path.join(os.path.dirname(__file__), "downloads")
+USE_PROXY = False
+PROXY = "http://127.0.0.1:8888" if USE_PROXY else None
